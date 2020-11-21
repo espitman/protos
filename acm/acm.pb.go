a0e3909b5d6ffb262a48d13ab88ae174ed8a2539
Saeed Heidari <saeed.heidari86@gmail.com>
2020-11-21 16:12:07 +0330

    acm update

--- acm/acm.pb.go
+++ acm/acm.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // 	protoc-gen-go v1.25.0
 // 	protoc        v3.7.1
-// source: acm.proto
+// source: acm/acm.proto
 
 package acm
 
@@ -29,31 +29,32 @@ const (
 // of the legacy proto package is being used.
 const _ = proto.ProtoPackageIsVersion4
 
-type Request struct {
+type RequestCreate struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
 	Title string `protobuf:"bytes,1,opt,name=title,proto3" json:"title,omitempty"`
+	Host  int32  `protobuf:"varint,2,opt,name=host,proto3" json:"host,omitempty"`
 }
 
-func (x *Request) Reset() {
-	*x = Request{}
+func (x *RequestCreate) Reset() {
+	*x = RequestCreate{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_acm_proto_msgTypes[0]
+		mi := &file_acm_acm_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
 }
 
-func (x *Request) String() string {
+func (x *RequestCreate) String() string {
 	return protoimpl.X.MessageStringOf(x)
 }
 
-func (*Request) ProtoMessage() {}
+func (*RequestCreate) ProtoMessage() {}
 
-func (x *Request) ProtoReflect() protoreflect.Message {
-	mi := &file_acm_proto_msgTypes[0]
+func (x *RequestCreate) ProtoReflect() protoreflect.Message {
+	mi := &file_acm_acm_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -64,133 +65,199 @@ func (x *Request) ProtoReflect() protoreflect.Message {
 	return mi.MessageOf(x)
 }
 
-// Deprecated: Use Request.ProtoReflect.Descriptor instead.
-func (*Request) Descriptor() ([]byte, []int) {
-	return file_acm_proto_rawDescGZIP(), []int{0}
+// Deprecated: Use RequestCreate.ProtoReflect.Descriptor instead.
+func (*RequestCreate) Descriptor() ([]byte, []int) {
+	return file_acm_acm_proto_rawDescGZIP(), []int{0}
 }
 
-func (x *Request) GetTitle() string {
+func (x *RequestCreate) GetTitle() string {
 	if x != nil {
 		return x.Title
 	}
 	return ""
 }
 
-type Response struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	Id    int32  `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
-	Title string `protobuf:"bytes,2,opt,name=title,proto3" json:"title,omitempty"`
-	Host  int32  `protobuf:"varint,3,opt,name=host,proto3" json:"host,omitempty"`
-}
-
-func (x *Response) Reset() {
-	*x = Response{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_acm_proto_msgTypes[1]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *Response) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*Response) ProtoMessage() {}
-
-func (x *Response) ProtoReflect() protoreflect.Message {
-	mi := &file_acm_proto_msgTypes[1]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use Response.ProtoReflect.Descriptor instead.
-func (*Response) Descriptor() ([]byte, []int) {
-	return file_acm_proto_rawDescGZIP(), []int{1}
-}
-
-func (x *Response) GetId() int32 {
-	if x != nil {
-		return x.Id
-	}
-	return 0
-}
-
-func (x *Response) GetTitle() string {
-	if x != nil {
-		return x.Title
-	}
-	return ""
-}
-
-func (x *Response) GetHost() int32 {
+func (x *RequestCreate) GetHost() int32 {
 	if x != nil {
 		return x.Host
 	}
 	return 0
 }
 
-var File_acm_proto protoreflect.FileDescriptor
+type RequestDetails struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
 
-var file_acm_proto_rawDesc = []byte{
-	0x0a, 0x09, 0x61, 0x63, 0x6d, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x03, 0x61, 0x63, 0x6d,
-	0x22, 0x1f, 0x0a, 0x07, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x14, 0x0a, 0x05, 0x74,
-	0x69, 0x74, 0x6c, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05, 0x74, 0x69, 0x74, 0x6c,
-	0x65, 0x22, 0x44, 0x0a, 0x08, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x0e, 0x0a,
-	0x02, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x05, 0x52, 0x02, 0x69, 0x64, 0x12, 0x14, 0x0a,
-	0x05, 0x74, 0x69, 0x74, 0x6c, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05, 0x74, 0x69,
-	0x74, 0x6c, 0x65, 0x12, 0x12, 0x0a, 0x04, 0x68, 0x6f, 0x73, 0x74, 0x18, 0x03, 0x20, 0x01, 0x28,
-	0x05, 0x52, 0x04, 0x68, 0x6f, 0x73, 0x74, 0x32, 0x36, 0x0a, 0x0a, 0x41, 0x63, 0x6d, 0x53, 0x65,
-	0x72, 0x76, 0x69, 0x63, 0x65, 0x12, 0x28, 0x0a, 0x07, 0x44, 0x65, 0x74, 0x61, 0x69, 0x6c, 0x73,
-	0x12, 0x0c, 0x2e, 0x61, 0x63, 0x6d, 0x2e, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x0d,
-	0x2e, 0x61, 0x63, 0x6d, 0x2e, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x00, 0x62,
-	0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	Id string `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
+}
+
+func (x *RequestDetails) Reset() {
+	*x = RequestDetails{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_acm_acm_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *RequestDetails) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*RequestDetails) ProtoMessage() {}
+
+func (x *RequestDetails) ProtoReflect() protoreflect.Message {
+	mi := &file_acm_acm_proto_msgTypes[1]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use RequestDetails.ProtoReflect.Descriptor instead.
+func (*RequestDetails) Descriptor() ([]byte, []int) {
+	return file_acm_acm_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *RequestDetails) GetId() string {
+	if x != nil {
+		return x.Id
+	}
+	return ""
+}
+
+type ResponseDetails struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Id    string `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
+	Title string `protobuf:"bytes,2,opt,name=title,proto3" json:"title,omitempty"`
+	Host  int32  `protobuf:"varint,3,opt,name=host,proto3" json:"host,omitempty"`
+}
+
+func (x *ResponseDetails) Reset() {
+	*x = ResponseDetails{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_acm_acm_proto_msgTypes[2]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *ResponseDetails) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*ResponseDetails) ProtoMessage() {}
+
+func (x *ResponseDetails) ProtoReflect() protoreflect.Message {
+	mi := &file_acm_acm_proto_msgTypes[2]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use ResponseDetails.ProtoReflect.Descriptor instead.
+func (*ResponseDetails) Descriptor() ([]byte, []int) {
+	return file_acm_acm_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *ResponseDetails) GetId() string {
+	if x != nil {
+		return x.Id
+	}
+	return ""
+}
+
+func (x *ResponseDetails) GetTitle() string {
+	if x != nil {
+		return x.Title
+	}
+	return ""
+}
+
+func (x *ResponseDetails) GetHost() int32 {
+	if x != nil {
+		return x.Host
+	}
+	return 0
+}
+
+var File_acm_acm_proto protoreflect.FileDescriptor
+
+var file_acm_acm_proto_rawDesc = []byte{
+	0x0a, 0x0d, 0x61, 0x63, 0x6d, 0x2f, 0x61, 0x63, 0x6d, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12,
+	0x03, 0x61, 0x63, 0x6d, 0x22, 0x39, 0x0a, 0x0d, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x43,
+	0x72, 0x65, 0x61, 0x74, 0x65, 0x12, 0x14, 0x0a, 0x05, 0x74, 0x69, 0x74, 0x6c, 0x65, 0x18, 0x01,
+	0x20, 0x01, 0x28, 0x09, 0x52, 0x05, 0x74, 0x69, 0x74, 0x6c, 0x65, 0x12, 0x12, 0x0a, 0x04, 0x68,
+	0x6f, 0x73, 0x74, 0x18, 0x02, 0x20, 0x01, 0x28, 0x05, 0x52, 0x04, 0x68, 0x6f, 0x73, 0x74, 0x22,
+	0x20, 0x0a, 0x0e, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x44, 0x65, 0x74, 0x61, 0x69, 0x6c,
+	0x73, 0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x02, 0x69,
+	0x64, 0x22, 0x4b, 0x0a, 0x0f, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x44, 0x65, 0x74,
+	0x61, 0x69, 0x6c, 0x73, 0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09,
+	0x52, 0x02, 0x69, 0x64, 0x12, 0x14, 0x0a, 0x05, 0x74, 0x69, 0x74, 0x6c, 0x65, 0x18, 0x02, 0x20,
+	0x01, 0x28, 0x09, 0x52, 0x05, 0x74, 0x69, 0x74, 0x6c, 0x65, 0x12, 0x12, 0x0a, 0x04, 0x68, 0x6f,
+	0x73, 0x74, 0x18, 0x03, 0x20, 0x01, 0x28, 0x05, 0x52, 0x04, 0x68, 0x6f, 0x73, 0x74, 0x32, 0x7a,
+	0x0a, 0x0a, 0x41, 0x63, 0x6d, 0x53, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x12, 0x34, 0x0a, 0x06,
+	0x43, 0x72, 0x65, 0x61, 0x74, 0x65, 0x12, 0x12, 0x2e, 0x61, 0x63, 0x6d, 0x2e, 0x52, 0x65, 0x71,
+	0x75, 0x65, 0x73, 0x74, 0x43, 0x72, 0x65, 0x61, 0x74, 0x65, 0x1a, 0x14, 0x2e, 0x61, 0x63, 0x6d,
+	0x2e, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x44, 0x65, 0x74, 0x61, 0x69, 0x6c, 0x73,
+	0x22, 0x00, 0x12, 0x36, 0x0a, 0x07, 0x44, 0x65, 0x74, 0x61, 0x69, 0x6c, 0x73, 0x12, 0x13, 0x2e,
+	0x61, 0x63, 0x6d, 0x2e, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x44, 0x65, 0x74, 0x61, 0x69,
+	0x6c, 0x73, 0x1a, 0x14, 0x2e, 0x61, 0x63, 0x6d, 0x2e, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73,
+	0x65, 0x44, 0x65, 0x74, 0x61, 0x69, 0x6c, 0x73, 0x22, 0x00, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74,
+	0x6f, 0x33,
 }
 
 var (
-	file_acm_proto_rawDescOnce sync.Once
-	file_acm_proto_rawDescData = file_acm_proto_rawDesc
+	file_acm_acm_proto_rawDescOnce sync.Once
+	file_acm_acm_proto_rawDescData = file_acm_acm_proto_rawDesc
 )
 
-func file_acm_proto_rawDescGZIP() []byte {
-	file_acm_proto_rawDescOnce.Do(func() {
-		file_acm_proto_rawDescData = protoimpl.X.CompressGZIP(file_acm_proto_rawDescData)
+func file_acm_acm_proto_rawDescGZIP() []byte {
+	file_acm_acm_proto_rawDescOnce.Do(func() {
+		file_acm_acm_proto_rawDescData = protoimpl.X.CompressGZIP(file_acm_acm_proto_rawDescData)
 	})
-	return file_acm_proto_rawDescData
+	return file_acm_acm_proto_rawDescData
 }
 
-var file_acm_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
-var file_acm_proto_goTypes = []interface{}{
-	(*Request)(nil),  // 0: acm.Request
-	(*Response)(nil), // 1: acm.Response
+var file_acm_acm_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
+var file_acm_acm_proto_goTypes = []interface{}{
+	(*RequestCreate)(nil),   // 0: acm.RequestCreate
+	(*RequestDetails)(nil),  // 1: acm.RequestDetails
+	(*ResponseDetails)(nil), // 2: acm.ResponseDetails
 }
-var file_acm_proto_depIdxs = []int32{
-	0, // 0: acm.AcmService.Details:input_type -> acm.Request
-	1, // 1: acm.AcmService.Details:output_type -> acm.Response
-	1, // [1:2] is the sub-list for method output_type
-	0, // [0:1] is the sub-list for method input_type
+var file_acm_acm_proto_depIdxs = []int32{
+	0, // 0: acm.AcmService.Create:input_type -> acm.RequestCreate
+	1, // 1: acm.AcmService.Details:input_type -> acm.RequestDetails
+	2, // 2: acm.AcmService.Create:output_type -> acm.ResponseDetails
+	2, // 3: acm.AcmService.Details:output_type -> acm.ResponseDetails
+	2, // [2:4] is the sub-list for method output_type
+	0, // [0:2] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
 	0, // [0:0] is the sub-list for extension extendee
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_acm_proto_init() }
-func file_acm_proto_init() {
-	if File_acm_proto != nil {
+func init() { file_acm_acm_proto_init() }
+func file_acm_acm_proto_init() {
+	if File_acm_acm_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_acm_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*Request); i {
+		file_acm_acm_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*RequestCreate); i {
 			case 0:
 				return &v.state
 			case 1:
@@ -201,8 +268,20 @@ func file_acm_proto_init() {
 				return nil
 			}
 		}
-		file_acm_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*Response); i {
+		file_acm_acm_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*RequestDetails); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_acm_acm_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*ResponseDetails); i {
 			case 0:
 				return &v.state
 			case 1:
@@ -218,20 +297,20 @@ func file_acm_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_acm_proto_rawDesc,
+			RawDescriptor: file_acm_acm_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   3,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
-		GoTypes:           file_acm_proto_goTypes,
-		DependencyIndexes: file_acm_proto_depIdxs,
-		MessageInfos:      file_acm_proto_msgTypes,
+		GoTypes:           file_acm_acm_proto_goTypes,
+		DependencyIndexes: file_acm_acm_proto_depIdxs,
+		MessageInfos:      file_acm_acm_proto_msgTypes,
 	}.Build()
-	File_acm_proto = out.File
-	file_acm_proto_rawDesc = nil
-	file_acm_proto_goTypes = nil
-	file_acm_proto_depIdxs = nil
+	File_acm_acm_proto = out.File
+	file_acm_acm_proto_rawDesc = nil
+	file_acm_acm_proto_goTypes = nil
+	file_acm_acm_proto_depIdxs = nil
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -246,7 +325,8 @@ const _ = grpc.SupportPackageIsVersion6
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type AcmServiceClient interface {
-	Details(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error)
+	Create(ctx context.Context, in *RequestCreate, opts ...grpc.CallOption) (*ResponseDetails, error)
+	Details(ctx context.Context, in *RequestDetails, opts ...grpc.CallOption) (*ResponseDetails, error)
 }
 
 type acmServiceClient struct {
@@ -257,8 +337,17 @@ func NewAcmServiceClient(cc grpc.ClientConnInterface) AcmServiceClient {
 	return &acmServiceClient{cc}
 }
 
-func (c *acmServiceClient) Details(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error) {
-	out := new(Response)
+func (c *acmServiceClient) Create(ctx context.Context, in *RequestCreate, opts ...grpc.CallOption) (*ResponseDetails, error) {
+	out := new(ResponseDetails)
+	err := c.cc.Invoke(ctx, "/acm.AcmService/Create", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *acmServiceClient) Details(ctx context.Context, in *RequestDetails, opts ...grpc.CallOption) (*ResponseDetails, error) {
+	out := new(ResponseDetails)
 	err := c.cc.Invoke(ctx, "/acm.AcmService/Details", in, out, opts...)
 	if err != nil {
 		return nil, err
@@ -268,14 +357,18 @@ func (c *acmServiceClient) Details(ctx context.Context, in *Request, opts ...grp
 
 // AcmServiceServer is the server API for AcmService service.
 type AcmServiceServer interface {
-	Details(context.Context, *Request) (*Response, error)
+	Create(context.Context, *RequestCreate) (*ResponseDetails, error)
+	Details(context.Context, *RequestDetails) (*ResponseDetails, error)
 }
 
 // UnimplementedAcmServiceServer can be embedded to have forward compatible implementations.
 type UnimplementedAcmServiceServer struct {
 }
 
-func (*UnimplementedAcmServiceServer) Details(context.Context, *Request) (*Response, error) {
+func (*UnimplementedAcmServiceServer) Create(context.Context, *RequestCreate) (*ResponseDetails, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Create not implemented")
+}
+func (*UnimplementedAcmServiceServer) Details(context.Context, *RequestDetails) (*ResponseDetails, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Details not implemented")
 }
 
@@ -283,8 +376,26 @@ func RegisterAcmServiceServer(s *grpc.Server, srv AcmServiceServer) {
 	s.RegisterService(&_AcmService_serviceDesc, srv)
 }
 
+func _AcmService_Create_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(RequestCreate)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(AcmServiceServer).Create(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/acm.AcmService/Create",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(AcmServiceServer).Create(ctx, req.(*RequestCreate))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 func _AcmService_Details_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(Request)
+	in := new(RequestDetails)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -296,7 +407,7 @@ func _AcmService_Details_Handler(srv interface{}, ctx context.Context, dec func(
 		FullMethod: "/acm.AcmService/Details",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(AcmServiceServer).Details(ctx, req.(*Request))
+		return srv.(AcmServiceServer).Details(ctx, req.(*RequestDetails))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -306,10 +417,14 @@ var _AcmService_serviceDesc = grpc.ServiceDesc{
 	HandlerType: (*AcmServiceServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
+			MethodName: "Create",
+			Handler:    _AcmService_Create_Handler,
+		},
+		{
 			MethodName: "Details",
 			Handler:    _AcmService_Details_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "acm.proto",
+	Metadata: "acm/acm.proto",
 }
