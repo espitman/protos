b0fdafbf2de16f7316c5fc66bea7a9d9647c4636
Saeed Heidari <saeed.heidari86@gmail.com>
2020-12-01 23:16:26 +0330

    acm update

--- acm/acm.methoods.go
+++ acm/acm.methoods.go
@@ -2,7 +2,7 @@ package acm
 
 import "encoding/json"
 
-func (t *RequestCreate) Convert(entry interface{}) RequestCreate {
+func (t *RequestCreate) UnmarshalBatch(entry interface{}) RequestCreate {
 	obj, _ := json.Marshal(entry)
 	response := RequestCreate{}
 	_ = json.Unmarshal(obj, &response)
