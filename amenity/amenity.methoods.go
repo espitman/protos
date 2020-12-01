706f88ceda4ae2a65c13159ba595ecfec150d60f
Saeed Heidari <saeed.heidari86@gmail.com>
2020-12-01 23:45:41 +0330

    amenity update

--- amenity/amenity.methoods.go
+++ amenity/amenity.methoods.go
@@ -8,3 +8,24 @@ func (t *RequestExist) Unmarshal(entry interface{}) RequestExist {
 	_ = json.Unmarshal(obj, &response)
 	return response
 }
+
+func (t *RequestCreate) Unmarshal(entry interface{}) RequestCreate {
+	obj, _ := json.Marshal(entry)
+	response := RequestCreate{}
+	_ = json.Unmarshal(obj, &response)
+	return response
+}
+
+func (t *ResponseDetails) Unmarshal(entry interface{}) ResponseDetails {
+	obj, _ := json.Marshal(entry)
+	response := ResponseDetails{}
+	_ = json.Unmarshal(obj, &response)
+	return response
+}
+
+func (t *ResponseFindByIds) Unmarshal(entry interface{}) ResponseFindByIds {
+	obj, _ := json.Marshal(entry)
+	response := ResponseFindByIds{}
+	_ = json.Unmarshal(obj, &response)
+	return response
+}
